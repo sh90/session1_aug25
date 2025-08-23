0114bec9e5375cfae1ff296ed6dacae95d035a02
sh90 <shikhatyagi1990@gmail.com>
2025-08-23 17:39:48 +0530

    Create ollama.py

---
+++ ollama.py
@@ -0,0 +1,14 @@
+# pip install ollama
+# Import packages
+import ollama
+
+# Name of the downloaded model
+model = "gemma3:1b"
+
+# Task Description
+task = """Write a 50 word summary on ethics in AI."""
+
+# Generate the result
+result = ollama.generate(model=model, task=task)
+
+print(result.response)
