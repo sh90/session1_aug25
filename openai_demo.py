520816a9592650f140910316e8dce783536d32b1
sh90 <shikhatyagi1990@gmail.com>
2025-08-23 19:16:57 +0530

    Update openai_demo.py

--- openai_demo.py
+++ openai_demo.py
@@ -1,12 +1,27 @@
-import ollama
+# pip install python-dotenv openai
 
-# Name of the downloaded model
-model = "gemma3:1b"
+# Import packages
+
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+#  Load .env from your working directory (or give an explicit path)
+load_dotenv()
+# Get openai key from environment file
+openai_key = os.getenv("OPENAI_API_KEY")
 
 # Task Description
 task = """Write a 50 word summary on ethics in AI."""
 
-# Generate the result
-result = ollama.generate(model=model, task=task)
+# Initialize openai
+client = OpenAI(api_key=openai_key)
 
-print(result.response)
+# Call GPT-4o-mini model
+response = client.responses.create(
+    model="gpt-4o-mini", # Name of the openai model
+    input=task # Instruction or prompt which will be passed to GPT model
+)
+
+# Read the response
+print(response.output_text)
